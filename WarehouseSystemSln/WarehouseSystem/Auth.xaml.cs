68b4ff14ff8e822248652d24ddea36b557a13181
Alexander Markov <markovalex2004@gmail.com>
2023-10-12 17:35:21 +0300

    login button pressed function added

--- WarehouseSystemSln/WarehouseSystem/Auth.xaml.cs
+++ WarehouseSystemSln/WarehouseSystem/Auth.xaml.cs
@@ -12,17 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WarehouseSystem.ViewModels;
 
 namespace WarehouseSystem
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new LoginViewModel();
         }
+
     }
 }
---
+++ WarehouseSystemSln/WarehouseSystem/ViewModels/LoginViewModel.cs
@@ -0,0 +1,47 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WarehouseSystem.ViewModels
+{
+    class LoginViewModel : INotifyPropertyChanged
+    {
+        private string _errorTextBlockValue = "";
+        public ICommand LoggingButtonPressed {  get; set; }
+        
+        public LoginViewModel()
+        {
+            LoggingButtonPressed = new RelayCommand(ExecuteLogin);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public void ExecuteLogin()
+        {
+            ErrorTextBlockValue = "Кнопка нажата";
+        }
+
+        public string ErrorTextBlockValue
+        {
+            get { return _errorTextBlockValue; }
+            set
+            {
+                _errorTextBlockValue = value;
+                OnPropertyChanged(nameof(ErrorTextBlockValue));
+            }
+        }
+    }
+}
