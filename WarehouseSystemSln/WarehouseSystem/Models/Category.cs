397df90270ecdab88718b4bf8d38e1b16cf44a1d
Alexander Markov <markovalex2004@gmail.com>
2023-10-27 23:34:55 +0300

    Categories styles fixed

--- WarehouseSystemSln/WarehouseSystem/Models/Category.cs
+++ WarehouseSystemSln/WarehouseSystem/Models/Category.cs
@@ -8,7 +8,7 @@ namespace WarehouseSystem.Models
 {
     public class Category
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
     }
