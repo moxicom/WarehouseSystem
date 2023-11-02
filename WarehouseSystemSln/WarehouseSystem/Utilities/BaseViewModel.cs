7b930eb4eb0c2540f12b59965a25cfa7ce7d83ad
Alexander Markov <markovalex2004@gmail.com>
2023-11-02 13:05:16 +0300

    utilities cleanup

--- WarehouseSystemSln/WarehouseSystem/Utilities/BaseViewModel.cs
+++ WarehouseSystemSln/WarehouseSystem/Utilities/BaseViewModel.cs
@@ -1,23 +1,15 @@
-using GalaSoft.MvvmLight;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using WarehouseSystem.Services;
+using GalaSoft.MvvmLight;
 
-namespace WarehouseSystem.Utilities
+namespace WarehouseSystem.Utilities;
+
+public abstract class BaseViewModel : ViewModelBase, INotifyPropertyChanged
 {
-    public abstract class BaseViewModel : ViewModelBase, INotifyPropertyChanged
-    {
-        public new event PropertyChangedEventHandler?  PropertyChanged;
+    public new event PropertyChangedEventHandler? PropertyChanged;
 
-        public void OnPropertyChanged([CallerMemberName] string prop = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
-        }
+    public void OnPropertyChanged([CallerMemberName] string prop = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
     }
 }
- 
--- WarehouseSystemSln/WarehouseSystem/Utilities/ConfirmationDialog.cs
+++ WarehouseSystemSln/WarehouseSystem/Utilities/ConfirmationDialog.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows;
 
-namespace WarehouseSystem.Utilities
+namespace WarehouseSystem.Utilities;
+
+internal class ConfirmationDialog
 {
-    internal class ConfirmationDialog
+    public async Task<bool> ShowConfirmationDialog(string message)
     {
-        public async Task<bool> ShowConfirmationDialog(string message) {
-            var result = MessageBox.Show(message, "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question);
-            return result == MessageBoxResult.Yes;
-        }
+        var result = MessageBox.Show(message, "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+        return result == MessageBoxResult.Yes;
     }
 }
